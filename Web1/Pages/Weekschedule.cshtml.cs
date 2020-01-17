81e0c9fd424a870c1e4329f6477a07e8c497df01
SNVU21 <snvu21@gmail.com>
2020-01-17 12:20:53 +0100

    first commit

---
+++ Web1/Pages/Weekschedule.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Web1.Pages
+{
+    public class WeekModel : PageModel
+    {
+        private readonly ILogger<WeekModel> _logger;
+        public IEnumerable<Week> Weeks { get; set; }
+        public WeekModel(ILogger<WeekModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Debug.WriteLine("weekschedule get");
+            Weeks = LoadJson();
+        }
+        public List<Week> LoadJson()
+        {
+            List<Week> items = Enumerable.Empty<Week>().ToList();
+            using (StreamReader r = new StreamReader("Wochen.json"))
+            {
+                string json = r.ReadToEnd();
+                 items = JsonConvert.DeserializeObject<List<Week>>(json);
+            }
+
+            return items;
+        }
+    }
+    public class Week
+    {
+        public int id;
+        public string Thema;
+        public string Dozent;
+        public string Raum;
+    }
+    public class Date
+    {
+        public static void CurrentDate()
+        {
+            DateTime thisDay = DateTime.Today;
+            var CurrentMonth = thisDay.ToString("MM/yyyy");
+         }
+
+    }
+
+}
